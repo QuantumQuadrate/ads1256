637da6748a431d170633fdf6751a07f6f4c3fc24
Matt Ebert <mfe5003@gmail.com>
2018-04-24 22:03:11 -0500

    minor fixes

--- ADS1256.py
+++ ADS1256.py
@@ -9,28 +9,29 @@ import os.path
 
 MAX_DATA_RATE = 30000  # SPS
 
+
 def read_diff(channel, log2_avgs):
-	if(channel >= 4 or channel < 0):
-		raise KeyError
-	if log2_avgs > 15:
-		log2avgs = 15
+    if(channel >= 4 or channel < 0):
+        raise KeyError
+    if log2_avgs > 15:
+        log2_avgs = 15
         script = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'ads1256_read_diff')
-	p = Popen(map(str, [script, channel, log2_avgs]), stdout=PIPE)
-	output, err = p.communicate()
-	output = output.strip()
-	if p.returncode:
-		print("Process exited on error with msg: `{}`".format(output))
-		raise IOError
-	return float(output)
+    p = Popen(map(str, [script, channel, log2_avgs]), stdout=PIPE)
+    output, err = p.communicate()
+    output = output.strip()
+    if p.returncode:
+        print("Process exited on error with msg: `{}`".format(output))
+        raise IOError
+    return float(output)
 
 
 if __name__ == "__main__":
-	# run test
-	channel = 0
-	log2_avgs = 4  # 2**4 = 16 avgs
-	try:
-		volt = read_diff(channel, log2_avgs)
-	except IOError:
-		print("Error reading ADC")
-	else:
-		print("ADC channel: {}, voltage {} V".format(channel, volt))
+    # run test
+    channel = 0
+    log2_avgs = 4  # 2**4 = 16 avgs
+    try:
+        volt = read_diff(channel, log2_avgs)
+    except IOError:
+        print("Error reading ADC")
+    else:
+        print("ADC channel: {}, voltage {} V".format(channel, volt))
